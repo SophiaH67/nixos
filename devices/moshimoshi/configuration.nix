f37dfe35b3057d9b9817d0b6687520d671cf6c7e
Sophia Hage <sophia@sophiah.gay>
2025-09-06 10:26:43 +0200

    feat: netboot proxy

--- devices/moshimoshi/configuration.nix
+++ devices/moshimoshi/configuration.nix
@@ -1,6 +1,7 @@
 {
   networking.hostName = "moshimoshi";
 
+  # -=-=- Asterisk -=-=-
   services.asterisk = {
     enable = true;
     confFiles = {
@@ -16,4 +17,13 @@
   ];
   networking.firewall.allowedUDPPorts = [ 5060 5061 ];
   networking.firewall.allowedUDPPortRanges = [ { from = 10000; to = 20000; } ];
+
+  # -=-=- Netboot -=-=-
+  services.pixiecore = {
+    enable = true;
+    openFirewall = true;
+    dhcpNoBind = false;
+    debug = true;
+    kernel = "https://boot.netboot.xyz";
+  };
 }

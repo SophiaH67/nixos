d58fef501f362f1b855f40e723805a0a212c1668
Sophia Hage <sophia@sophiah.gay>
2025-09-16 23:55:23 +0200

    fix: give schwi an ip address (oops)

--- devices/kube/nodes/schwi.nix
+++ devices/kube/nodes/schwi.nix
@@ -3,6 +3,11 @@
 
   services.ex-machina.enable = true;
   services.ex-machina.init = true;
+  
+  networking.interfaces.enp3s0.ipv6.addresses = [{
+    address = "2a02:810d:6f83:ad00:acab::1";
+    prefixLength = 64;
+  }];
 
   networking.hostName = "schwi";
   networking.domain = "ex-machina.sophiah.gay";

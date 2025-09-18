d7ecf8d49f12eabe5872efb4f464f8fec62ee420
Sophia Hage <sophia@sophiah.gay>
2025-09-18 22:09:03 +0200

    refactor: idek

--- devices/kube/common.nix
+++ devices/kube/common.nix
@@ -1,15 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  networking.firewall.enable = true;
-  environment.systemPackages = with pkgs; [
-    vim
-    curl
-    wget
-  ];
-
-  services.k3s.enable = true;
   
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 }
--- devices/kube/master.nix
+++ devices/kube/master.nix
@@ -1,34 +1,32 @@
 { pkgs, lib, config, ... }:
-with lib;
 let
   cfg = config.services.ex-machina;
 in {
-  imports = [ ./common.nix ];
+  # imports = [ ./common.nix ];
 
   options.services.ex-machina = {
-    enable = mkEnableOption "ex-machina";
+    enable = lib.mkEnableOption "ex-machina";
 
-    init = mkOption {
-      type = types.bool;
+    init = lib.mkOption {
+      type = lib.types.bool;
       default = false;
       description = "Whether this build will initialise the cluster.";
     };
 
-    initialMasterIp = mkOption {
-      type = types.str;
+    virtualIp = lib.mkOption {
+      type = lib.types.str;
       description = "The IP address of the initial master node to join. Only used if init is false.";
-      example = "2a02:810d:6f83:ad00:1ac0:4dff:fed9:df52";
+      default = "2a02:810d:6f83:ad00:3ac1::67";
     };
   };
 
-  config = mkIf cfg.enable {
-    services.k3s.role = "server";
+  config = lib.mkIf cfg.enable {
     services.keepalived = {
       enable = true;
       vrrpInstances.k3s = {
-        interface = "eth0";
+        interface = "enp3s0";
         virtualIps = [{
-          addr = "2a02:810d:6f83:ad00:3ac1::67";
+          addr = cfg.virtualIp;
           }]; 
         virtualRouterId = 67;
         priority = if cfg.init then 200 else 100;
@@ -36,13 +34,17 @@ in {
       };
     };
 
-    services.k3s.extraFlags = toString ([
-      "--disable traefik"
-      "--node-taint node-role.kubernetes.io/master=true:NoSchedule"
-      
-      
-    ] ++ optionals cfg.init [ "--cluster-init" ] ++ optionals (!cfg.init && !empty(cfg.initialMasterIp)) [ "--server https://${cfg.initialMasterIp}:6443" ]);
+    networking.firewall.allowedTCPPorts = [ 6443 6444 2379 2380 10250 10251 10252 ];
 
-    services.k3s.clusterInit = true; # HA etcd
+    services.k3s = {
+      role = "server";
+      extraFlags = "--disable traefik --debug";
+      enable = true;
+    } // (if cfg.init then {
+      clusterInit = true;
+    } else {
+      # serverAddr = "https://${cfg.virtualIp}:6443";
+      serverAddr = "https://[2a02:810d:6f83:ad00:acab::1]:6443";
+    });
   };
 }

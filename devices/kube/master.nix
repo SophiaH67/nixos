fb8fdb5a780e4e6fd85707b0e4ca13243a49ad19
Sophia Hage <sophia@sophiah.gay>
2025-09-20 23:30:56 +0200

    feat: working cluster

--- devices/kube/master.nix
+++ devices/kube/master.nix
@@ -30,27 +30,45 @@ in {
     networking.firewall.allowedTCPPorts = [ 6443 6444 2379 2380 10250 10251 10252 ];
 
     virtualisation.containerd.enable = true;
+    # Longhorn dependencies
+    services.openiscsi.enable = true;
+    environment.systemPackages = [ pkgs.nfs-utils ];
+
+    # Symlink the loopback plugin into /opt/cni/bin, as containerd expects to find it there.
+    # This is needed for flannel to work correctly.
+    systemd.tmpfiles.rules = [
+      "L+ /opt/cni/bin/loopback - - - - ${pkgs.cni-plugins}/bin/loopback"
+      # Same with bridge plugin
+      "L+ /opt/cni/bin/bridge - - - - ${pkgs.cni-plugins}/bin/bridge"
+      # And host-local IPAM plugin
+      "L+ /opt/cni/bin/host-local - - - - ${pkgs.cni-plugins}/bin/host-local"
+      # portmap
+      "L+ /opt/cni/bin/portmap - - - - ${pkgs.cni-plugins}/bin/portmap"
+    ];
 
     services.k3s = {
       role = "server";
       extraFlags = [
         "--disable traefik"
+        "--flannel-backend none"
         "--tls-san ${cfg.virtualIp}"
         "--node-ip ${cfg.nodeIp}"
         "--node-external-ip ${cfg.nodeIp}"
-        "--flannel-external-ip ${cfg.nodeIp}"
-        # "--kube-controller-manager-arg cluster-cidr=fd00:3ac1::/56"
         "--cluster-cidr=fd00:3ac1::/56"
-        "--service-cidr=fd00:3ac1:7::/112"
-        "--flannel-ipv6-masq"
-        "--flannel-backend host-gw"
-        "--cluster-domain ex-machina.sophiah.gay"
+        # Specify a pod CIDR to ensure flannel uses it
+        "--kube-controller-manager-arg=allocate-node-cidrs=true"
+        "--kube-controller-manager-arg=cluster-cidr=fd00:3ac1::/56"
+        "--kube-controller-manager-arg=node-cidr-mask-size-ipv6=64"
       ];
       enable = true;
       token = "exmachinampXeJcPsGKDFgapj";
       manifests = {
         kube-vip-rbac = {
           source = ./manifests/kube-vip.yaml;
+          enable = true;
+        };
+        flannel = {
+          source = ./manifests/kube-flannel.yml;
           enable = true;
         };
       };
--- devices/kube/nodes/emir-eins.nix
+++ devices/kube/nodes/emir-eins.nix
@@ -1,7 +1,7 @@
 {
   imports = [ ../master.nix ];
 
-  services.ex-machina.enable = true; # Todo re-enable with join token
+  services.ex-machina.enable = true;
   
   networking.interfaces.enp3s0.ipv6.addresses = [{
     address = "2a02:810d:6f83:ad00:acab::2";

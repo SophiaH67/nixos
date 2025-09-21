8cb845e859a12c5cd4460aac4e5522e85437f9c6
Sophia Hage <sophia@sophiah.gay>
2025-09-22 00:00:10 +0200

    fix(moshimoshi): please work radio

--- devices/moshimoshi/configuration.nix
+++ devices/moshimoshi/configuration.nix
@@ -24,7 +24,8 @@
   ];
 
   environment.etc."radio.sh" = {
-    source = ./radio.sh;
+    content = ''#!/bin/sh
+${pkgs.ffmpeg}/bin/ffmpeg -i http://listener3.mp3.tb-group.fm:80/hb.mp3 -ar 8000 -ac 1 -f mulaw -'';
     mode = "0755";
   };
 
--- devices/moshimoshi/radio.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ffmpeg -i http://listener3.mp3.tb-group.fm:80/hb.mp3 -ar 8000 -ac 1 -f mulaw - | cat

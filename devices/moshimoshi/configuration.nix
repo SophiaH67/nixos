ff24008d4b6f4c5b53df41aa0f1a044b49d8f6f2
Sophia Hage <sophia@sophiah.gay>
2025-09-22 21:02:00 +0200

    dev(moshimoshi): attempt 2 at radio

--- devices/moshimoshi/configuration.nix
+++ devices/moshimoshi/configuration.nix
@@ -19,14 +19,25 @@
 
   # Hardstyle radio
   environment.systemPackages = with pkgs; [
-    mpg123
     ffmpeg
   ];
-
-  environment.etc."radio.sh" = {
-    text = ''#!/bin/sh
-${pkgs.ffmpeg}/bin/ffmpeg -hide_banner -loglevel error -i http://listener3.mp3.tb-group.fm:80/hb.mp3 -ar 8000 -ac 1 -f mulaw - 2> /dev/null | cat'';
-    mode = "0755";
+  # Sytemd service to:
+  # 1) Make a fifo for asterisk (/etc/asterisk/sounds/custom/hardstyle-radio.ulaw)
+  # 2) Use ffmpeg to read from the hardstyle radio stream and output to the fifo
+  systemd.services.hardstyle-radio = {
+    description = "Hardstyle Radio Stream for Asterisk";
+    after = [ "network.target" ];
+    wants = [ "network.target" ];
+    serviceConfig = {
+      ExecStartPre = "${pkgs.coreutils}/bin/mkfifo /tmp/hardstyleRadio.ulaw";
+      # ExecStart = "${pkgs.ffmpeg}/bin/ffmpeg -i https://listener2.mp3.tb-group.fm/hb.mp3 -ar 8000 -ac 1 -f mulaw pipe:1 > /tmp/hardstyleRadio.ulaw";
+      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.ffmpeg}/bin/ffmpeg -i https://streaming.radio.co/s95f5f4f4a/listen -ar 8000 -ac 1 -f mulaw pipe:1 > /tmp/hardstyleRadio.ulaw'";
+      ExecStopPost = "${pkgs.coreutils}/bin/rm -f /tmp/hardstyleRadio.ulaw";
+      Restart = "always";
+      RestartSec = "10";
+      User = "asterisk";
+      Group = "asterisk";
+    };
   };
 
   # -=-=- Netboot -=-=-

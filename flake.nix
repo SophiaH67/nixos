e920acd0a8bb393fe32c8fd2b53f7abce1c4e647
Sophia Hage <sophia@sophiah.gay>
2025-10-09 21:51:03 +0200

    refactor: change way of installing agenix pkg

--- flake.nix
+++ flake.nix
@@ -44,6 +44,10 @@
           ./common/sophia-dev.nix
           home-manager.nixosModules.home-manager
           lanzaboote.nixosModules.lanzaboote
+          agenix.nixosModules.default
+          {
+            environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
+          }
         ];
       };
 
@@ -59,6 +63,9 @@
           home-manager.nixosModules.home-manager
           lanzaboote.nixosModules.lanzaboote
           agenix.nixosModules.default
+          {
+            environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
+          }
         ];
       };
 

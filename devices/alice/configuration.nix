13b22f7311f6e8970a165388740d950f0a88100e
Sophia Hage <shage2@allegion.com>
2025-10-13 12:05:30 +0200

    feat: nix-ld for gdb

--- devices/alice/configuration.nix
+++ devices/alice/configuration.nix
@@ -27,4 +27,6 @@
   age.secrets.secret1.file = ../../secrets/secret1.age;
   environment.etc."secret1".source = config.age.secrets.secret1.path;
   environment.etc."secret1.src".text = config.age.secrets.secret1.path;
+
+  programs.nix-ld.enable = true; # Needed for vscode gdb to work
 }

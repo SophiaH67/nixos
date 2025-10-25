05f7502b622d7884b790cf04f33f578df64b766a
Sophia Hage <sophia@sophiah.gay>
2025-10-25 22:39:42 +0200

    fix(vr): alcom needs some weird env to make the gui work

--- common/apps/vr-dev.nix
+++ common/apps/vr-dev.nix
@@ -1,7 +1,13 @@
 { pkgs, config, ...}: 
 {
   environment.systemPackages = with pkgs; [
-    alcom
+    # https://github.com/vrc-get/vrc-get/issues/1405
+    (alcom.overrideAttrs (oldAttrs: {
+      postFixup = (oldAttrs.postFixup or "") + ''
+        wrapProgram $out/bin/ALCOM \
+          --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+      '';
+    }))
     unityhub # Unfortunately no direct unity from nixpkgs :/
   ];
 }

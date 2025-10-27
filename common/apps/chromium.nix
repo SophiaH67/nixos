8525e9ed134aba114b1b18da16d7c8482fd14296
Sophia Hage <sophia@sophiah.gay>
2025-10-27 08:22:30 +0100

    feat(chromium): enable manifest V2 and WideVine DRM

--- common/apps/chromium.nix
+++ common/apps/chromium.nix
@@ -2,6 +2,13 @@
 {
   home-manager.users.sophia.programs.chromium = {
     enable = true;
+    # From https://github.com/hyblocker/nixfiles/blob/master/apps/browsers.nix
+    package = (pkgs.chromium.override {
+      enableWideVine = true;
+      commandLineArgs = [
+        "--disable-features=ExtensionManifestV2Unsupported,ExtensionManifestV2Disabled"
+      ];
+    });
     dictionaries = [
       # pkgs.hunspellDicts.tok
       pkgs.hunspellDictsChromium.en_US

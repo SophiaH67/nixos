226ae2efdb864ae8c0be6fec151e58c107d32d39
Sophia Hage <sophia@sophiah.gay>
2025-11-02 16:32:49 +0100

    fix(ayumu): disable boot-unlock

--- devices/ayumu/configuration.nix
+++ devices/ayumu/configuration.nix
@@ -6,7 +6,7 @@
   environment.systemPackages = [ pkgs.spotify ];
 
   sophices.tailscale.enable = true;
-  sophices.boot-unlock.enable = true;
+  sophices.boot-unlock.enable = false;
   sophices.boot-unlock.tor = true;
   soph.drawing.enable = true;
 

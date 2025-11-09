5884529562f05724420b3b8df5dde69533e5e14b
Sophia Hage <sophia@sophiah.gay>
2025-11-09 11:59:45 +0100

    fix(rikka): filme-serien.iceportal.de wrong ip

--- nixosConfigurations/rikka/configuration.nix
+++ nixosConfigurations/rikka/configuration.nix
@@ -56,7 +56,7 @@
 172.18.1.110  iceportal.de
 172.18.1.110  zugportal.de
 172.18.1.110  www.iceportal.de
-172.18.1.110 filme-serien.iceportal.de
+172.18.1.100 filme-serien.iceportal.de
   '';
 
   services.fprintd.enable = true;

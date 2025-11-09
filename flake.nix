3d29c040838bd3a860c399e3555a00e75645b64b
Sophia Hage <sophia@sophiah.gay>
2025-11-09 11:09:55 +0100

    build: update flake lock

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
     nixcord.url = "github:kaylorben/nixcord";
     nixcord.inputs.nixpkgs.follows = "nixpkgs";
 
-    vrcft.url = "git+https://xn--55q89qy6p.com/Soph/VRCFaceTracking.Avalonia.git?submodules=1";
+    vrcft.url = "github:dfgHiatus/VRCFaceTracking.Avalonia?submodules=1";
     vrcft.inputs.nixpkgs.follows = "nixpkgs";
 
     kubenix.url = "github:hall/kubenix";

84d23632698830da6cf8b89c8198d99250cc4c26
Sophia Hage <sophia@sophiah.gay>
2025-12-09 19:55:14 +0100

    refactor(syncthing): :recycle: move syncthing to new homeManagerModules

--- common/sophia-gui.nix
+++ common/sophia-gui.nix
@@ -31,6 +31,11 @@
   home-manager.useGlobalPkgs = true;
   home-manager.backupFileExtension = "bak";
   home-manager.users.sophia = { pkgs, ... }: {
+    imports = [
+      ../homeManagerModules
+    ];
+
+    sophices.syncthing.enable = true;
 
     home.packages = with pkgs; [
       atool
@@ -252,38 +257,6 @@
         enableSshSupport = false;
         sshKeys = [ "1FB01D6AA1106525" ];
         maxCacheTtl = 300;
-      };
-      syncthing = {
-        enable = true;
-        overrideDevices = false;
-        overrideFolders = false;
-        guiAddress = "[::1]:8384";
-        settings = {
-          devices = {
-            mococo = {
-              id = "6IPUJU4-PX3VFSP-EZ3PNAU-7CVOXAG-476QTZB-4DEORJB-NB4B2XQ-WF72BAN";
-            };
-            rikka = {
-              id = "H57XCEM-AE6HEL7-AOXSSIY-4JDLLCD-PZTAQFB-QQZBX2C-7UQVDM6-5QBAVAM";
-            };
-            ayumu = {
-              id = "3WOL2NN-YEMHJEJ-KQ3NWIZ-2ZVEK6S-N3ADXL3-VNPJCKB-3ZLFBEY-FIFYQQX";
-            };
-            tyrants_eye = {
-              id = "A3QLL5C-ARZUF27-VKUT5OY-6ZAVJS4-55VGXZ6-EGKH2AB-OVQZSUU-GCXGVQX";
-            };
-            alice = {
-              id = "RXLKWVK-WRPWLP7-7ECMV7D-OJTTV6A-OTTDSX7-FFPLH7N-FDQLNBQ-FNUPGAV";
-            };
-          };
-          folders = {
-            "/home/sophia/sync" = {
-              id = "nei9h-knicz";
-              devices = [ "alice" "mococo" "ayumu" "rikka" ];
-              label = "Soph's Nix Syncing";
-            };
-          };
-        };
       };
     };
   };
---
+++ homeManagerModules/services/syncthing/default.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ...}:
+{
+  options.sophices.syncthing.enable = lib.mkEnableOption "Soph Syncthing";
+
+  config = lib.mkIf config.sophices.syncthing.enable {
+    services.syncthing = {
+      enable = true;
+      overrideDevices = false;
+      overrideFolders = false;
+      guiAddress = "[::1]:8384";
+      settings = {
+        devices = {
+          mococo = {
+            id = "6IPUJU4-PX3VFSP-EZ3PNAU-7CVOXAG-476QTZB-4DEORJB-NB4B2XQ-WF72BAN";
+          };
+          rikka = {
+            id = "H57XCEM-AE6HEL7-AOXSSIY-4JDLLCD-PZTAQFB-QQZBX2C-7UQVDM6-5QBAVAM";
+          };
+          ayumu = {
+            id = "3WOL2NN-YEMHJEJ-KQ3NWIZ-2ZVEK6S-N3ADXL3-VNPJCKB-3ZLFBEY-FIFYQQX";
+          };
+          tyrants_eye = {
+            id = "A3QLL5C-ARZUF27-VKUT5OY-6ZAVJS4-55VGXZ6-EGKH2AB-OVQZSUU-GCXGVQX";
+          };
+          alice = {
+            id = "RXLKWVK-WRPWLP7-7ECMV7D-OJTTV6A-OTTDSX7-FFPLH7N-FDQLNBQ-FNUPGAV";
+          };
+        };
+        folders = {
+          "/home/sophia/sync" = {
+            id = "nei9h-knicz";
+            devices = [ "alice" "mococo" "ayumu" "rikka" ];
+            label = "Soph's Nix Syncing";
+          };
+        };
+      };
+    };
+  };
+}

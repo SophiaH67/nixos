5e5168db0abd27ccc30ca398849a6ff14e0168b8
Sophia Hage <shage2@allegion.com>
2025-12-10 14:32:37 +0100

    refactor(niri): :recycle: move niri config to kdl file

--- nixosModules/programs/niri/default.nix
+++ nixosModules/programs/niri/default.nix
@@ -20,69 +20,7 @@
     home-manager.users.sophia =
       { pkgs, ... }:
       {
-        programs.niri.settings = {
-          environment = {
-            "NIXOS_OZONE_WL" = "1";
-          }
-          // config.environment.sessionVariables;
-
-          input = {
-            keyboard = {
-              xkb = {
-                layout = "us";
-              };
-              numlock = true;
-            };
-
-            touchpad = {
-              scroll-method = "two-finger";
-              natural-scroll = true;
-            };
-          };
-
-          layout = {
-            gaps = 16;
-            center-focused-column = "never";
-
-            preset-column-widths = [
-              { proportion = 0.33333; }
-              { proportion = 0.5; }
-              { proportion = 0.66667; }
-            ];
-
-            default-column-width = { };
-
-            focus-ring = {
-              enable = true;
-              width = 4;
-              active.color = "#7fc8ff";
-              inactive.color = "#505050";
-            };
-          };
-
-          binds = {
-            "Mod+1".action.focus-workspace = 1;
-            "Mod+2".action.focus-workspace = 2;
-            "Mod+3".action.focus-workspace = 3;
-            "Mod+4".action.focus-workspace = 4;
-            "Mod+5".action.focus-workspace = 5;
-            "Mod+6".action.focus-workspace = 6;
-            "Mod+7".action.focus-workspace = 7;
-            "Mod+8".action.focus-workspace = 8;
-            "Mod+9".action.focus-workspace = 9;
-
-            "Mod+Shift+Left".action.focus-monitor = "left";
-            "Mod+Shift+Down".action.focus-monitor = "down";
-            "Mod+Shift+Up".action.focus-monitor = "up";
-            "Mod+Shift+Right".action.focus-monitor = "right";
-            "Mod+Shift+H".action.focus-monitor = "left";
-            "Mod+Shift+J".action.focus-monitor = "down";
-            "Mod+Shift+K".action.focus-monitor = "up";
-            "Mod+Shift+L".action.focus-monitor = "right";
-
-            "Mod+F".action.maximize-window-to-edges = { };
-          };
-        };
+        programs.niri.config = builtins.readFile ./config.kdl;
       };
   };
 }

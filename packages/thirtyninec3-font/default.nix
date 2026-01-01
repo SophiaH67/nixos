e68a0d971e5891930789cb8b812462aa8e2f80e2
Sophia Hage <sophia@sophiah.gay>
2026-01-01 12:39:18 +0100

    fix(thirtyninec3-font): :fire: remove wrong line

--- packages/thirtyninec3-font/default.nix
+++ packages/thirtyninec3-font/default.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 pkgs.stdenvNoCC.mkDerivation {
   name = "thirtyninec3-font";
-  dontConfigue = true;
   src = pkgs.fetchzip {
     url = "https://events.ccc.de/congress/2025/infos/styleguide/39c3-styleguide-full-latest.zip";
     sha256 = "sha256-rMG8Km4EYJnC0MVaUXQsOiDGQt6Nhlj35UbUUorNH7E=";

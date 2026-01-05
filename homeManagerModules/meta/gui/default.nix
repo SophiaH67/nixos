97fa1a7221cb0ba6c5fbb2385acd7c4b64a04d93
Sophia Hage <sophia@sophiah.gay>
2026-01-05 14:22:28 +0100

    fix(sophia-gui): :bug: fix element config not being applied

--- homeManagerModules/meta/gui/default.nix
+++ homeManagerModules/meta/gui/default.nix
@@ -40,27 +40,25 @@
     programs = {
       element-desktop = {
         enable = true;
-        settings = ''
-          {
-            default_server_config = {
-              "m.homeserver" = {
-                  base_url = "https://cat.sophiah.gay";
-                  server_name = "cat.sophiah.gay";
-              };
-              "m.identity_server" = {
-                  base_url = "https://vector.im";
-              };
+        settings = {
+          default_server_config = {
+            "m.homeserver" = {
+              base_url = "https://cat.sophiah.gay";
+              server_name = "cat.sophiah.gay";
             };
-            disable_custom_urls = false;
-            disable_guests = false;
-            disable_login_language_selector = false;
-            disable_3pid_login = false;
-            force_verification = false;
-            brand = "Element";
-            integrations_ui_url = "https://scalar.vector.im/";
-            integrations_rest_url = "https://scalar.vector.im/api";
-          }
-        '';
+            "m.identity_server" = {
+              base_url = "https://vector.im";
+            };
+          };
+          disable_custom_urls = false;
+          disable_guests = false;
+          disable_login_language_selector = false;
+          disable_3pid_login = false;
+          force_verification = false;
+          brand = "Element";
+          integrations_ui_url = "https://scalar.vector.im/";
+          integrations_rest_url = "https://scalar.vector.im/api";
+        };
       };
       neovim = {
         enable = true;

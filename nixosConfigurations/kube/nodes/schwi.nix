3f15f94d5763c7c9b42a7ded62c504fbc003551d
Sophia Hage <shage2@allegion.com>
2026-01-26 13:15:21 +0100

    feat(schwi): :sparkles: add gui to schwi

--- nixosConfigurations/kube/nodes/schwi.nix
+++ nixosConfigurations/kube/nodes/schwi.nix
@@ -10,12 +10,18 @@
       prefixLength = 64;
     }
   ];
+
   networking.interfaces.br0.ipv4.addresses = [
     {
       address = "192.168.178.211";
       prefixLength = 24;
     }
   ];
+
+  home-manager.users.sophia = {
+    soph.gui.enable = true;
+    sophrams.gnome.enable = true;
+  };
 
   networking.hostName = "schwi";
   networking.domain = "ex-machina.sophiah.gay";

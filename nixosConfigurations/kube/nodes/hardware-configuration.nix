7891d82b404a5184b12700b658ea52d4842da3bd
Sophia Hage <shage2@allegion.com>
2026-01-26 16:37:37 +0100

    feat(ex-machina): :sparkles: copy gpu example from https://wiki.nixos.org/wiki/Intel_Graphics

--- nixosConfigurations/kube/nodes/hardware-configuration.nix
+++ nixosConfigurations/kube/nodes/hardware-configuration.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
@@ -15,4 +15,26 @@
       interface = "br0";
     };
   };
+
+  # https://wiki.nixos.org/wiki/Intel_Graphics
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # VA-API (iHD) userspace
+      vpl-gpu-rt # oneVPL (QSV) runtime
+
+      # Optional (compute / tooling):
+      intel-compute-runtime # OpenCL (NEO) + Level Zero for Arc/Xe
+      # NOTE: 'intel-ocl' also exists as a legacy package; not recommended for Arc/Xe.
+      # libvdpau-va-gl       # Only if you must run VDPAU-only apps
+    ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD"; # Prefer the modern iHD backend
+    # VDPAU_DRIVER = "va_gl";      # Only if using libvdpau-va-gl
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  boot.kernelParams = [ "i915.enable_guc=3" ];
 }

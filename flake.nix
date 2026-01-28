a3cbb1a7ae4e05f973a0e421a72d575709bac1eb
Sophia Hage <shage2@allegion.com>
2026-01-28 10:09:28 +0100

    build: :arrow_up: switch to nixos-unstable and flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";

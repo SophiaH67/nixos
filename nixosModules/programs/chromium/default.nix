5b8483073d5478158f0a7e7d172768decfd0410e
Sophia Hage <shage2@allegion.com>
2026-02-03 20:31:31 +0100

    feat(chromium): :sparkles: bookmarks

--- nixosModules/programs/chromium/default.nix
+++ nixosModules/programs/chromium/default.nix
@@ -36,10 +36,32 @@
         ];
         ManagedBookmarks = [
           {
+            toplevel_name = "Awruff wruff :3";
+          }
+          # Soph Stuff
+          {
             name = "公民身份识别系统";
-            url = "https://xn--15qt0w.xn--55q89qy6p.com/";
+            url = "https://xn--15qt0w.xn--55q89qy6p.com/settings/apps";
+          }
+          {
+            name = "源控";
+            url = "https://xn--55q89qy6p.com/";
+          }
+          {
+            name = "国内公民规划系统";
+            url = "https://xn--wbrs17k.xn--55q89qy6p.com/";
+          }
+          # Misc
+          {
+            name = "Fedi";
+            url = "https://chaos.social";
+          }
+          {
+            name = "Yuri";
+            url = "https://twitter.com/";
           }
         ];
+        BookmarkBarEnabled = true;
 
         # https://github.com/wimpysworld/nix-config/blob/92fcaeef7a87b608f7e041522d4627f31a8e6086/nixos/_mixins/desktop/apps/web-browsers/martin.nix#L35
         "HomePageLocation" = "https://kagi.com";

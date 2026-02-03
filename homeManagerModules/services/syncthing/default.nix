d3afd4624ba3f3a8d9ad8db62918316671b2125d
Sophia Hage <sophia@sophiah.gay>
2026-02-03 21:53:05 +0100

    feat(syncthing): :sparkles: add keepassxc

--- homeManagerModules/services/syncthing/default.nix
+++ homeManagerModules/services/syncthing/default.nix
@@ -42,6 +42,14 @@
             ];
             label = "Soph's Nix Syncing";
           };
+          "/home/sophia/KeePassXC" = {
+            id = "0k33p-2ssxc";
+            devices = [
+              "ayumu"
+              "rikka"
+            ];
+            label = "Keepass XC";
+          };
         };
       };
     };

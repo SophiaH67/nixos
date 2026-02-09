d73cb4e82a82c2d97dd7f7fc7f8a051eb31c8402
Sophia Hage <shage2@allegion.com>
2026-02-09 14:24:11 +0100

    feat(alice): :sparkles: add emulator crt

--- nixosConfigurations/alice/configuration.nix
+++ nixosConfigurations/alice/configuration.nix
@@ -57,4 +57,6 @@
   programs.nix-ld.enable = true; # Needed for vscode gdb to work
   sophices.tailscale.enable = lib.mkForce false;
   sophices.renovate.enable = lib.mkForce false;
+
+  security.pki.certificateFiles = [ ./emulator.crt ];
 }

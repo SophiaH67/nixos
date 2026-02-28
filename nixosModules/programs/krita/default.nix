c3c280f38b048bb0e3ecf9f95167165a1ea59a6a
Sophia Hage <sophia@sophiah.gay>
2026-02-28 11:59:37 +0100

    fix(rikka): :rotating_light: build issues with nixpkgs
    Thanks nixpkgs maintainers for never bothering to check if you break stuff. Amazing!!!

--- nixosModules/programs/krita/default.nix
+++ nixosModules/programs/krita/default.nix
@@ -8,6 +8,6 @@
   options.sophrams.krita.enable = lib.mkEnableOption "Soph Krita";
 
   config = lib.mkIf config.sophrams.krita.enable {
-    environment.systemPackages = with pkgs; [ krita ];
+    # environment.systemPackages = with pkgs; [ krita ];
   };
 }
